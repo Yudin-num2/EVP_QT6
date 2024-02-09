4208e0b184bc33e57ba586e902db4f3a3ab28b14
yudin <brawlakkum@gmail.com>
2024-02-10 00:06:36 +0300

    complete auth with postgre

--- database.py
+++ database.py
@@ -1,2 +1,27 @@
-import asyncpg
+import psycopg
+import configparser
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+USERNAME = config.get('Postgre', 'user')
+PASSWORD = config.get('Postgre', 'password')
+DATABASE = config.get('Postgre', 'database')
+
+def authorization(login: str, passw: str) -> bool:
+    try:
+        with psycopg.connect(host="127.0.0.1",
+                              port=5432,
+                              user=USERNAME,
+                              password=PASSWORD,
+                              dbname=DATABASE) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""SELECT * FROM users_authorization WHERE login = %s AND passw = %s""", (login, passw))
+                return True if cur.fetchall() else False
+
+    except Exception as exc:
+        logging.warning(f'[WARNING] Ошибка работы с БД: {exc}')
 
--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMessageBox
 from login_form import Ui_login_form
+from database import *
 
 
 class Login(QWidget):
@@ -8,9 +9,21 @@ class Login(QWidget):
         super().__init__()
 
         self.ui = Ui_login_form()       
-        self.ui.setupUi(self)       
-        
+        self.ui.setupUi(self)
+
+        self.ui.btn_login.clicked.connect(self.check_authorization)
         self.show()
+
+    def check_authorization(self):
+        login = self.ui.login_text.text()
+        passw = self.ui.password_text.text()
+        
+        result = authorization(login=login, passw=passw)
+
+        if result:
+            QMessageBox.information(self, "Успешная авторизация", "Вы успешно авторизовались")
+        else:
+            QMessageBox.warning(self, "Ошибка авторизации", "Неверный логин или пароль")
     
 
 if __name__ == '__main__':
